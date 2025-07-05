f7d9a898072fa80753fe42332566025e026e7202
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-07-06 01:04:00 +0500

    loads initial load to display homepage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 console.log("Running")
 import "./styles.css"
+import { homeDiv } from "./initial-load"
+
+const content = document.querySelector("#content")
+content.appendChild(homeDiv)
---
+++ src/initial-load.js
@@ -0,0 +1,9 @@
+const homeDiv = document.createElement("div")
+const homeText = document.createElement("h1");
+
+homeDiv.className = "front-image";
+homeText.textContent= "LOUNGE\nCAFE";
+
+homeDiv.appendChild(homeText)
+
+export {homeDiv}
