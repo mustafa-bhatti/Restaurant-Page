377dd8fcca890d4ac53e9b9fe7a0771fdedd2e48
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-07-06 17:47:32 +0500

    fix webpack config errors.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
  const { merge } = require('webpack-merge');
  const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-};
+});
